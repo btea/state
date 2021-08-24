336a8f7bbe28a35594e213bcf6f4e322f26027f4
btea <2356281422@qq.com>
2021-08-24 16:20:08 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="box">
+		<input type="text" v-model="input" />
+		<button @click="add">点击+1</button>
+		<div>结果为：{{ input }}</div>
+	</div>
+</template>
+<script setup>
+import { ref } from '@vue/composition-api';
+const input = ref('');
+const add = () => {
+	const v = +input.value;
+	input.value = v + 1;
+};
+</script>
