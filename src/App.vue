7563a62d649ea051c5ea1423a2a5a9fb1ff00bc3
btea <2356281422@qq.com>
2021-08-25 11:31:14 +0800

    vue2-setup

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
 		<input type="text" v-model="input" />
 		<button @click="add">点击+1</button>
 		<div>结果为：{{ input }}</div>
+		<hello-world :message="input"></hello-world>
 	</div>
 </template>
 <script setup>
 import { ref } from '@vue/composition-api';
+import HelloWorld from './hellloWorld.vue';
 const input = ref('');
 const add = () => {
 	const v = +input.value;
-	input.value = v + 1;
+	input.value = v + 1 + '';
 };
 </script>
---
+++ src/hellloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>the message is {{ message }}</div>
+</template>
+<script setup>
+const props = defineProps({
+	message: String,
+});
+console.log(props);
+</script>
